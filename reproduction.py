520f427a751873ab0b1e3b06bc1a59e384509783
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-06 22:19:47 +0200

    Add guard clause, change var names

--- reproduction.py
+++ reproduction.py
@@ -11,51 +11,48 @@ def reproduce(males, females, offspring, lifespan):
     children (male and female objects).
     """
     # males and females ready for reproduction
-    readyMales = [m for m in males if not m.dead]
-    readyFemales = [f for f in females if not f.dead]
-    smaller_pop = min(len(readyMales), len(readyFemales))
-    newMales, newFemales = [], []
+    ready_males = [m for m in males if not m.dead]
+    ready_females = [f for f in females if not f.dead]
+    new_males, new_females = [], []
 
     # loop through the smaller population in size
-    for i in range(smaller_pop):
-        # if the male is not sterile
-        if not readyMales[i].sterile:
-            # prepare a pool of children
-            children = [
-                random.choice(("male", "female"))
-                for child in range(random.randrange(offspring + 1))
-            ]
-            # if the female parent has the CRISPR gene
-            # female children have CRISPR and male children are sterile
-            if readyFemales[i].crispr:
-                for s in children:
-                    if s == "male":
-                        # new sterile male with random lifespan and age 0
-                        newMales.append(
-                            Male(
-                                lifespan=random.randrange(1, lifespan + 1), sterile=True
-                            )
-                        )
-                    if s == "female":
-                        # new CRISPR female with random lifespan and age 0
-                        newFemales.append(
-                            Female(
-                                lifespan=random.randrange(1, lifespan + 1), crispr=True
-                            )
-                        )
-            # if the female parent doesn't have the CRISPR gene
-            # then children are normal
-            elif not readyFemales[i].crispr:
-                for s in children:
-                    if s == "male":
-                        # new normal male with random lifespan and age 0
-                        newMales.append(
-                            Male(lifespan=random.randrange(1, lifespan + 1))
-                        )
-                    if s == "female":
-                        # new normal female with random lifespan and age 0
-                        newFemales.append(
-                            Female(lifespan=random.randrange(1, lifespan + 1))
-                        )
+    for male, female in zip(ready_males, ready_females):
 
-    return newMales, newFemales
+        # if male sterile skip this pair
+        if male.sterile:
+            continue
+
+        # prepare a pool of children
+        children = [
+            random.choice(("male", "female"))
+            for child in range(random.randrange(offspring + 1))
+        ]
+
+        # if the female parent has the CRISPR gene
+        # female children have CRISPR and male children are sterile
+        if female.crispr:
+            for s in children:
+                if s == "male":
+                    # new sterile male with random lifespan and age 0
+                    new_males.append(
+                        Male(lifespan=random.randrange(1, lifespan + 1), sterile=True)
+                    )
+                if s == "female":
+                    # new CRISPR female with random lifespan and age 0
+                    new_females.append(
+                        Female(lifespan=random.randrange(1, lifespan + 1), crispr=True)
+                    )
+        # if the female parent doesn't have the CRISPR gene
+        # then children are normal
+        elif not female.crispr:
+            for s in children:
+                if s == "male":
+                    # new normal male with random lifespan and age 0
+                    new_males.append(Male(lifespan=random.randrange(1, lifespan + 1)))
+                if s == "female":
+                    # new normal female with random lifespan and age 0
+                    new_females.append(
+                        Female(lifespan=random.randrange(1, lifespan + 1))
+                    )
+
+    return new_males, new_females
