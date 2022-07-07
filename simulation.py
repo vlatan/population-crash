25c02bc2429e7daa05ca993dc029dd029e0801a2
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-07 19:20:19 +0200

    Refactor

--- simulation.py
+++ simulation.py
@@ -3,43 +3,32 @@ from population import *
 from reproduction import *
 
 
-def simulate(cycles, initSize, offspring, lifespan, crisprFems, popLimit):
+def simulate():
     """
-    cycles: the number of cycles this simulation will run.
-    initSize: the initial size of the population.
-    offspring: max number of possible children per reproduction.
-    lifespan: maximum lifespan of an individual.
-    crisprFems: the initial number of CRISPR females in the population.
-    popLimit: the maximum size this population can get.
-    ---------------------------------------------------
     Records the relevant numbers after every cycle.
     Returns the numbers for each population category in a dict.
     """
     # build the population
-    males, females = create_population(initSize, lifespan, crisprFems)
+    males, females = create_population()
 
     # record the initial numbers
     population = len(males) + len(females)
-    totalPop, crispr, sterile = [population], [crisprFems], [0]
-    nonSterile, nonCrispr = [0], [len(females) - crisprFems]
+    total_pop, crispr, sterile = [population], [CRISPR_FEMALES], [0]
+    non_sterile, non_crispr = [0], [len(females) - CRISPR_FEMALES]
 
     # go through the given number of cycles
-    for cycle in range(cycles):
-        # refresh the population numbers
-        population = len(males) + len(females)
+    for _ in range(CYCLES):
         # if population hasn't reached the limit
-        if population < popLimit:
+        if population < POPULATION_LIMIT:
             # produce offspring
-            maleKids, femaleKids = reproduce(males, females, offspring, lifespan)
-            # select random sample from the offspring in this cycle
-            numChildren = min(
-                (popLimit - population) // 2, len(maleKids), len(femaleKids)
-            )
-            # add them to the population
-            males += random.sample(maleKids, numChildren)
-            random.shuffle(femaleKids)
-            females += random.sample(femaleKids, numChildren)
-        # increase age the end of every cycle
+            male_kids, female_kids = reproduce(males, females)
+            # add surviving children to fit the population
+            rest = (POPULATION_LIMIT - population) // 2
+            num_kids = min(rest, len(male_kids), len(female_kids))
+            males += random.sample(male_kids, num_kids)
+            females += random.sample(female_kids, num_kids)
+
+        # increase the age of the individuals
         for m in males:
             m.age += 1
         for f in females:
@@ -48,30 +37,27 @@ def simulate(cycles, initSize, offspring, lifespan, crisprFems, popLimit):
         # remove the dead individuals
         males = [m for m in males if not m.dead]
         females = [f for f in females if not f.dead]
-        # check how many CRISPR females are there
-        crisprF = [f for f in females if f.crispr]
-        # check how many sterile males are there
-        sterileM = [m for m in males if m.sterile]
-        # check how many healthy females are there
-        nonCrisprF = [f for f in females if not f.crispr]
-        # check how many healthy males are there
-        nonSterileM = [m for m in males if not m.sterile]
+        # refresh the population number
+        population = len(males) + len(females)
+
+        crispr_fems = [f for f in females if f.crispr]
+        sterile_males = [m for m in males if m.sterile]
+
+        # record (append) the population numbers for this cycle
+        crispr.append(len(crispr_fems))  # CRISPR females
+        non_crispr.append(len(females) - len(crispr_fems))  # NON CRISPR females
+        sterile.append(len(sterile_males))  # STERILE males
+        non_sterile.append(len(males) - len(sterile_males))  # NON STERILE males
+        total_pop.append(population)  # total population
 
         # shuffle males and females
         random.shuffle(males)
         random.shuffle(females)
 
-        # record (append) the population numbers for this cycle
-        crispr.append(len(crisprF))  # CRISPR females
-        sterile.append(len(sterileM))  # sterile males
-        nonSterile.append(len(nonSterileM))  # non STERILE males
-        nonCrispr.append(len(nonCrisprF))  # non CRISPR females
-        totalPop.append(population)  # total population
-
     return dict(
         crispr=crispr,
         sterile=sterile,
-        nonSterile=nonSterile,
-        nonCrispr=nonCrispr,
-        totalPop=totalPop,
+        non_sterile=non_sterile,
+        non_crispr=non_crispr,
+        total_pop=total_pop,
     )
