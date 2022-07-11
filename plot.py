290b91c359d48bc0c12d05f7e3232a5ab19284dd
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-11 15:45:13 +0200

    Customize figure

--- plot.py
+++ plot.py
@@ -7,6 +7,7 @@ def plot(non_sterile, non_crispr, crispr, sterile, total_pop):
     """Plots various different population categories."""
     plt.style.use("fivethirtyeight")
     mpl.rcParams["figure.figsize"] = [16, 9]
+    mpl.rcParams["figure.dpi"] = 90
     mpl.rcParams["axes.titlepad"] = 25
     mpl.rcParams["axes.titlesize"] = 15
     mpl.rcParams["axes.titleweight"] = 700
@@ -16,10 +17,6 @@ def plot(non_sterile, non_crispr, crispr, sterile, total_pop):
     mpl.rcParams["legend.fontsize"] = 14
     mpl.rcParams["xtick.labelsize"] = 14
     mpl.rcParams["ytick.labelsize"] = 14
-    mpl.rcParams["savefig.bbox"] = "tight"
-    mpl.rcParams["savefig.pad_inches"] = 0.8
-    # mpl.rcParams['savefig.dpi'] = 80
-    # mpl.rcParams['figure.dpi'] = 80
 
     plt.plot(non_sterile, lw=2, label="Healthy Males")
     plt.plot(non_crispr, lw=2, label="Healthy Females")
@@ -30,6 +27,7 @@ def plot(non_sterile, non_crispr, crispr, sterile, total_pop):
     plt.ylabel("Population")
     plt.title("Population with CRISPR introduced")
     plt.legend()
+    plt.tight_layout(pad=2.5)
     plt.show()
 
 
